45ca17da82613c25257bfa8022878df4707274dd
Roy <roygufo@gmail.com>
2024-05-01 15:26:45 -0600

    Add localizations configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:clubhub/core/extensions/string_extensions.dart';
 import 'package:flutter/material.dart';
+
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() => runApp(const MyApp());
 
@@ -7,15 +10,26 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
+    return const MaterialApp(
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
+      title: "ClubHub",
+      home: HomeWidget(),
+    );
+  }
+}
+
+class HomeWidget extends StatelessWidget {
+  const HomeWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(context.loc.appTitle),
+      ),
+      body: Center(
+        child: Text(context.loc.helloWorld),
       ),
     );
   }
